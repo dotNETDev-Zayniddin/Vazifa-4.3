7f030a1561844583049af536a7cc6687da1d9bae
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-05 19:35:14 +0500

    Vazifa: Task Upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Vazifa
+{
+    class Program
+    {
+        static void Main(){
+            System.Console.Write("Hafta kunini o'zbek tilida kiriting: ");
+            string day = Console.ReadLine().ToLower();
+
+
+            switch(day){
+                case "dushanba": System.Console.WriteLine("Monday");
+                break;
+                case "seshanba": System.Console.WriteLine("Tuesday");
+                break;
+                case "chorshanba": System.Console.WriteLine("Wednesday");
+                break;
+                case "payshanba": System.Console.WriteLine("Thursday");
+                break;
+                case "juma": System.Console.WriteLine("Friday");
+                break;
+                case "shanba": System.Console.WriteLine("Sunday");
+                break;
+                default: System.Console.WriteLine("Error input! Try again!");
+                break;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Vazifa-4.3
+Consoleda o’zbek tilida kiritilgan hafta kunini ingliz tiliga o’girib, 
+natijani ekranga chiqaradigan dastur tuzing. 
+Switch expression dan foydalaning!
